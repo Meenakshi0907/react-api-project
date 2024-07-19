51354a3764cb1a24bfd8170f23730772ed60f63f
Meenakshi0907 <meenakshiapril25@gmail.com>
2024-07-19 09:55:34 +0530

    Add-name,status

--- src/component/Display/Display.jsx
+++ src/component/Display/Display.jsx
@@ -15,6 +15,23 @@ const Display = () => {
   return (
     <div>
       <h1>Display</h1>
+      {data.length > 0 &&
+        data.map((data, item) => (
+          <div key={item}>
+            <h1>Name: {data.name}</h1>
+            <div style={{display: "flex", gap: "20px"}}>
+              <h3 >Id: {data.id}</h3>
+              <h3
+                style={{
+                  color: data.status === "Alive" ? "green" : "red",
+                }}
+              >
+                Status: {data.status}
+              </h3>
+            </div>
+            {/* <h3 key={item} style={ data.status === "Alive" ? {color:"green"}:{color:"red"}}>Status: {data.status}</h3> */}
+          </div>
+        ))}
     </div>
   );
 };
